6d1beec01472239820a6e82b62ff5d71f70341c8
gdulab <gdula.bartlomiej@gmail.com>
2023-06-02 15:48:14 +0200

    FIX code improvements

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,7 +4,8 @@ import CardForm from "../CardForm/CardForm"
 import { useSelector } from "react-redux";
 
 const Column = props => {
-    const cards = useSelector (state => state.cards.filter(card => card.columnId === props.id))
+    const searchString = useSelector (state => state.searchString);
+    const cards = useSelector (state => state.cards.filter(card => card.columnId === props.id && card.title.toLowerCase().includes(searchString.toLowerCase())))
     
     return (
         <article className={styles.column}>
@@ -15,7 +16,7 @@ const Column = props => {
             <ul className={styles.cards}>
                 {cards.map(card => <Card key={card.id} title={card.title}/>)}
             </ul>
-            <CardForm columnId={props.id} action={props.action}/>
+            <CardForm columnId={props.id}/>
         </article>
     )
 }
